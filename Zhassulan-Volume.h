e692bd1eb777b9ac786bb1594fb84e4c03abf22c
КеримханЖ <@N210-5>
2021-06-23 20:51:17 +0600

    Zhassulan

---
+++ Zhassulan-Volume.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <fstream>
+#include <vector>
+#include <Windows.h>
+#include <conio.h>
+using namespace std;
+class Music {
+private:
+	string name;
+	int like;
+	bool grammy;
+public:
+	Music(string name,int like, bool grammy){
+		this->name = name;
+		this->like = like;
+		this->grammy = grammy;
+	}
+	Music(){}
+	void print() {
+		cout << "Name :" << name << endl;
+		cout << "Like or Spotify :" << like << endl;
+		cout << "Grammy or " << boolalpha << grammy << endl;
+	}
+};
